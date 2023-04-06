fe83d152675a452f39c4ed3997c45c72f14c95af
wzp21142 <411314320@qq.com>
2023-04-06 20:34:48 +0800

    Add tqdm and a stupid large text files translating solution.

--- translate.py
+++ translate.py
@@ -1,7 +1,7 @@
 from utils.utils import *
 from dotenv.main import load_dotenv
 import argparse
-
+import tqdm
 load_dotenv() 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -13,25 +13,46 @@ args = parser.parse_args()
 
 inputPath,language,outputPath=args.i,args.l,args.o
 localeFiles=getAllymlFiles(inputPath)
+lang=getPromptLang(language)
+if outputPath is None:
+    outputPath=os.path.join(os.path.dirname(inputPath), language.lower())
+tokensInTotal=0
 
 
-count=0
-for file in localeFiles:
-    count+=1
+for file in tqdm.tqdm(localeFiles):
+    print(file.filePath)
     file.content=getKey_Value(file.content)
     origin=file.content[1]
-    messages=[
-    {"role": "system", "content": "You are a great translate bot. Translate a i18n locale array content to "+getPromptLang(language)+". It's a array structure, contains many strings, translate each of them and make a new array of translated strings. Consider all the string as a context to make better translation. Do not translate the text between [], $$ or @ and !.\n"},
-    {"role": "user", "content": "".join(origin.__str__())},
-]
-    print(str(messages)+"\n")
-    if tokensCount(messages)<=4095:
-        file.content[1]=translator(messages)[2:-2].split("', '")
-        print(file.content[1])
-        if outputPath is None:
-            outputPath=os.path.join(os.path.dirname(inputPath), language.lower())
-        file.content=getPairs(file.content[0],file.content[1])
-        saveFile(language,outputPath,file)
+    errorPath=os.path.join(outputPath,file.filePath+"_error.yml")
+    # The total tokens counter is erroneous
+    '''From OpenAI's documentation:
+    Note that very long conversations are more likely to receive incomplete replies. 
+    For example, a gpt-3.5-turbo conversation that is 4090 tokens long will have its reply cut off after just 6 tokens.
+    A tested configuration that works well on Victoria 3 game is 1350 tokens per request. If you want to change this, DO REMEMBER TO CHANGE THE VALUE IN utils.splitContents.
+    '''
+    get_text = lambda _t: "[["+"".join(getSeparatedContent(_t)).__str__()+"]]"
+    
+    _text=get_text(origin)
+    tokens=len(tiktoken.encoding_for_model('gpt-3.5-turbo-0301').encode(_text))
+    tokensInTotal+=tokens
+    if tokens<=1350:
+        file.content=segmentTranslator(lang,_text,file.content[0],errorPath)
     else:
-        print("Error: Too many tokens in "+file.filePath)
-print("Done")
+        splitLocation=splitContents(origin)
+        translated=[]
+        start=0
+        for i in range(len(splitLocation)):
+            if i==len(splitLocation)-1 and splitLocation[-1]==len(origin) and start!=0:
+                start=len(origin)-1
+            else:
+                #print(len(tiktoken.encoding_for_model('gpt-3.5-turbo').encode("".join(origin[start:splitLocation[i]-1].__str__()))))
+                _text=get_text(origin[start:(splitLocation[i]-1)])
+                translated.append(segmentTranslator(lang,_text,file.content[0][start:splitLocation[i]-1],errorPath,i,translated))
+                start=splitLocation[i]
+        _text=get_text(origin[start:])
+        translated.append(segmentTranslator(lang,_text,file.content[0][start:],errorPath,i,translated))
+        file.content=translated
+    saveFile(language,outputPath,file)
+
+
+print("Done, total tokens used:"+str(tokensInTotal))
--- utils/utils.py
+++ utils/utils.py
@@ -3,13 +3,30 @@ import os
 from utils.ymlFile import *
 import re
 import tiktoken
-def translator(_messages):
-    response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=_messages,
-                #temperature=0.2,
-            )
-    return response.choices[0].message.content.strip()
+def translator(lang,content):
+    messages=[
+        {"role": "system", "content": "You are a great translate bot. Translate a locale array content to "+lang+". It's an array structure which is related to politics and history, contains many strings that separated by UP arrow ↑, translate each of them and make a new array of translated strings separated by UP arrow ↑. Do make sure that translate every element between two ↑ and do not miss any of them. Do not translate the text between [], $$ or @ and !.\n"},
+        {"role": "user", "content": content},
+    ]
+    try:
+        response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=messages,
+                    #The hyperparameters may be optimizable to keep the stability of the text form
+                )
+    except openai.error.APIError as e:
+        #Handle API error here, e.g. retry or log
+        print(f"OpenAI API returned an API Error: {e}")
+        pass
+    except openai.error.APIConnectionError as e:
+        #Handle connection error here
+        print(f"Failed to connect to OpenAI API: {e}")
+        pass
+    except openai.error.RateLimitError as e:
+        #Handle rate limit error (we recommend using exponential backoff)
+        print(f"OpenAI API request exceeded rate limit: {e}")
+        pass
+    return response.choices[0].message.content.strip()[2:-2].split("', '")
 
 def getAllymlFiles(path):
     ymlFiles = []
@@ -26,10 +43,13 @@ def getKey_Value(content):
     content=content.split('\n')[1:]
     content=[s for s in content if len(s)>3]
     pattern = r':\d*\s"'
+    annoPattern = r'^\s*#'
     key,value=[],[]
     for i in content:
         searcher=re.search(pattern,i)
-        if searcher is None: continue
+        annoSearcher=re.search(annoPattern,i)
+        if (searcher is None) or (annoSearcher is not None):
+            continue
         start,end=searcher.start(),searcher.end()
         space=re.search(r'\s',i[start:end]).start()
         key.append(i[:start+space])
@@ -61,19 +81,40 @@ def saveFile(lang,path,files):
         print("Error: "+savePath)
         return False
 
-def tokensCount(messages):
-    """Returns the number of tokens used by a list of messages."""
+def splitContents(content):
+    # return the position where exceed 4096 tokens
+    start=0
+    ans=[]
+    for pos in range(len(content)):
+        if len(tiktoken.encoding_for_model('gpt-3.5-turbo-0301').encode("[["+"".join(getSeparatedContent(content[start:pos])).__str__()+"]]"))>1350:
+            ans.append(pos)
+            start=pos
+    return ans
+
+def getSeparatedContent(content):
+    ans=""
+    for pos in range(len(content)):
+        ans+=content[pos]
+        if pos != len(content)-1:
+            ans+='↑'
+    return ans
+
+def segmentTranslator(lang,_text,_keys,errorPath,segmentNum=0,translatedList=[]):
+    translated=translator(lang, _text)
+    translated="".join(translated).split("↑")
     try:
-        encoding = tiktoken.encoding_for_model('gpt-3.5-turbo')
-    except KeyError:
-        encoding = tiktoken.get_encoding("cl100k_base")
-    num_tokens = 0
-    for message in messages:
-        num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
-        for key, value in message.items():
-            num_tokens += len(encoding.encode(value))
-            if key == "name":  # if there's a name, the role is omitted
-                num_tokens += -1  # role is always required and always 1 token
-    num_tokens += 2  # every reply is primed with <im_start>assistant
-    print("tokens:"+str(num_tokens))
-    return num_tokens
+        ans=getPairs(_keys,translated)
+    except:
+        errorPath=os.path.join(errorPath+"_"+str(segmentNum))
+        if not os.path.exists(os.path.dirname(errorPath)):
+            os.makedirs(os.path.dirname(errorPath))
+        with open(errorPath, 'w', encoding='UTF-8') as f:
+            if len(translatedList)==0:
+                for i in range(len(translated)):
+                    translated[i]+='\n'
+                f.write(''.join(translated))
+            else:
+                f.write(''.join(translatedList))
+                f.write(''.join(translated))
+        exit("Error, the half-translated file is saved to "+errorPath)
+    return ans
